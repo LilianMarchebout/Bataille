fac24f31e52ff07ad14765dab411c283a08c0afa
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-11-07 18:33:12 +0100

    Update Bataille.py

--- Bataille.py
+++ Bataille.py
@@ -7,7 +7,7 @@ from random import randint
 
 ##VARIABLE
 
-card = dict() # Dictionnaire associant un nombre à une carte (numéro de la carte : [nom de la carte, nombre de carte prise])
+card = {1: ["Deux", 0], 2: ["Trois", 0]} # Dictionnaire associant un nombre à une carte (numéro de la carte : [nom de la carte, nombre de carte prise])
 
 
 # Liste des cartes des joueurs
@@ -23,3 +23,23 @@ cardNumber = 52 # Nombre de cartes dans le paquet de jeu
 
 
 ##SCRIPT
+
+# Attribution des cartes aux joueurs
+while len(cardP1) < 26 and len(cardP2) < 26:
+    # Joueur 1
+    cardRandom = randint(1, 13)
+    substitute = card[cardRandom]
+    substitute[1] += 1
+    while substitute[1] > 4:
+        cardRandom = randint(1, 13)
+        substitute = card[cardRandom]
+        substitute[1] -= 1
+    cardP1.append(cardRandom)
+    # Joueur 2
+    cardRandom = randint(1, 13)
+    substitute = card[cardRandom]
+    while substitute[1] > 4:
+        cardRandom = randint(1, 13)
+        substitute = card[cardRandom]
+        substitute[1] -= 1
+    cardP2.append(cardRandom)
