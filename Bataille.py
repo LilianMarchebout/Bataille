66219a9b2456371f2c26696e122d1014e36b87b2
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-11-07 18:15:54 +0100

    Update Bataille.py

--- Bataille.py
+++ Bataille.py
@@ -5,12 +5,12 @@ from random import randint
 
 
 ##VARIABLE
-card = dict() # Dictionnaire associant un nombre à une carte
+card = dict() # Dictionnaire associant un nombre à une carte (numéro de la carte : [nom de la carte, nombre de carte prise])
 
 
 # Liste des cartes des joueurs
-cardP1 = [] 
-cardP2 = []
+cardP1 = [] # Joueur 1
+cardP2 = [] # Joueur 2
 
 cardNumber = 52 # Nombre de cartes dans le paquet de jeu
 
