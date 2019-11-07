b46bef00293bc9e600f5e14aa335b9f1d1c4e65a
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-11-07 19:10:10 +0100

    Update Bataille.py

--- Bataille.py
+++ Bataille.py
@@ -7,7 +7,20 @@ from random import randint
 
 ##VARIABLE
 
-card = {1: ["Deux", 0], 2: ["Trois", 0]} # Dictionnaire associant un nombre à une carte (numéro de la carte : [nom de la carte, nombre de carte prise])
+# Dictionnaire associant un nombre à une carte (numéro de la carte : [nom de la carte, nombre de carte prise])
+card = {1: ["Deux", 0], 
+2: ["Trois", 0], 
+3: ["Quatre", 0], 
+4: ["Cinq", 0], 
+5: ["Six", 0], 
+6: ["Sept", 0], 
+7: ["Huit", 0], 
+8: ["Neuf", 0], 
+9: ["Dix", 0], 
+10: ["Valet", 0], 
+11: ["Dame", 0], 
+12: ["Roi", 0], 
+13: ["As", 0]} 
 
 
 # Liste des cartes des joueurs
@@ -24,22 +37,32 @@ cardNumber = 52 # Nombre de cartes dans le paquet de jeu
 
 ##SCRIPT
 
-# Attribution des cartes aux joueurs
-while len(cardP1) < 26 and len(cardP2) < 26:
-    # Joueur 1
-    cardRandom = randint(1, 13)
-    substitute = card[cardRandom]
-    substitute[1] += 1
-    while substitute[1] > 4:
+# Distribution des cartes aux joueurs
+def distribution():
+    while len(cardP1) < 26 and len(cardP2) < 26:
+        # Joueur 1
         cardRandom = randint(1, 13)
         substitute = card[cardRandom]
-        substitute[1] -= 1
-    cardP1.append(cardRandom)
-    # Joueur 2
-    cardRandom = randint(1, 13)
-    substitute = card[cardRandom]
-    while substitute[1] > 4:
+        substitute[1] += 1
+        while substitute[1] > 4:
+            substitute[1] -= 1
+            distribution()
+        cardP1.append(cardRandom)
+        # Joueur 2
         cardRandom = randint(1, 13)
-        substitute = card[cardRandom]
-        substitute[1] -= 1
-    cardP2.append(cardRandom)
+        while substitute[1] > 4:
+            substitute[1] -= 1
+            distribution()
+        cardP2.append(cardRandom)
+
+#Affichage
+distribution()
+print("liste P1 =")
+for boucle in range(len(cardP1)):
+    substitue = cardP1[boucle]
+    print(boucle , ":" , card[substitue])
+print("liste P2 =")
+for boucle in range(len(cardP2)):
+    substitue = cardP2[boucle]
+    print(boucle , ":" , card[substitue])
+print("disctionnaire = ", card)
