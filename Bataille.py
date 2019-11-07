c683c1a6d0263ba761cb4fa4d94dda91ed034442
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-11-07 21:11:32 +0100

    Update Bataille.py

--- Bataille.py
+++ Bataille.py
@@ -37,7 +37,7 @@ def distribution():
     """
     Distribution des cartes aux joueurs
     """
-    while len(cardP1) < cardNumber/2:
+    while len(cardP1) < cardNumber/2 and len(cardP2) < cardNumber/2:
         # Joueur 1
         cardRandom = randint(1, 13)
         substitute = card[cardRandom]
@@ -47,7 +47,6 @@ def distribution():
             distribution()
         else:
             cardP1.append(cardRandom)
-    while len(cardP2) < cardNumber/2:
         # Joueur 2
         cardRandom = randint(1, 13)
         substitute = card[cardRandom]
@@ -58,6 +57,24 @@ def distribution():
         else:
             cardP2.append(cardRandom)
 
+
+def round():
+    cardPlayP1 = cardP1[0]
+    cardPlayP2 = cardP2[0]
+    if cardPlayP1 > cardPlayP2: #Joueur 1 gagne la manche
+        # Met à la fin du paquet du joueur la carte jouée
+        cardP1.remove(cardPlayP1)
+        cardP1.append(cardPlayP1)
+        # Ajoute la carte gagné et la supprimme à l'adversaire
+        cardP2.remove(cardPlayP2)
+        cardP1.append(cardPlayP2)
+    if cardPlayP1 < cardPlayP2: #Joueur 2 gagne la manche
+        # Met à la fin du paquet du joueur la carte jouée
+        cardP2.remove(cardPlayP2)
+        cardP2.append(cardPlayP2)
+        # Ajoute la carte gagné et la supprimme à l'adversaire
+        cardP1.remove(cardPlayP1)
+        cardP2.append(cardPlayP1)
 
 
 
